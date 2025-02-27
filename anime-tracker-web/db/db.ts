b21e039d9501fbf91d829e6aaa1a4293832fcd32
n1ira <tk210250@tks.iput.ac.jp>
2025-02-27 11:37:06 +0900

    fix init

--- anime-tracker-web/db/db.ts
+++ anime-tracker-web/db/db.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-config({ path: ".env. local" });
+config({ path: ".env.local" });
 
 const schema = {};
 
